31aca9f5e11d44f4ccabb0a8b2772a5985812991
Dave Henton <davehenton@gmail.com>
2016-09-21 11:28:59 -0700

    Add questions users and answers controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+get '/users/new' do
+  erb :'/users/new'
+end
+
+post '/users/new' do
+  @user = User.new(params)
+  if @user.save
+    login(@user)
+
+    redirect "/users/#{@user.id}"
+  else
+    @error = "Sorry, you entered in some wrong information - please try again."
+
+    erb :'/users/new'
+  end
+end
+
+get '/users/:id' do
+  if session[:user_id]
+    @user = User.find(session[:user_id])
+  end
+
+  erb :'/users/show'
+end
+
+
+get '/users/delete' do
+  User.find(session[:user_id]).destroy
+  session[:user_id] = nil
+
+  redirect '/'
+end
