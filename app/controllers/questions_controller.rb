c79523f41427883684869f7cafe42168a1ebe018
Dave Henton <davehenton@gmail.com>
2016-09-21 16:20:29 -0700

    Debug iteration on questions index. Add redirect from root.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,9 @@
+get '/' do
+  redirect '/questions'
+end
+
+
+
 get '/questions' do
   @questions = Question.all 
   erb :'questions/index' 
@@ -19,14 +25,19 @@ get '/questions/:id/edit' do
   erb :'questions/edit' 
 end
 
-
 post '/questions' do
-  @question = Question.new(params[:question])
-    redirect '/questions' 
+
+  #below works with properly formatted params in HTML form
+  @question = Question.new(params[:question]) #create new question
+
+  if @question.save #saves new question or returns false if unsuccessful
+    redirect '/questions' #redirect back to questions index page
   else
-    erb :'questions/new' 
+    erb :'questions/new' # show new questions view again(potentially displaying errors)
   end
+
 end
+
 
 put '/questions/:id' do
   @question = Question.find(params[:id])
