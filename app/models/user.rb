9a3f519a6ff47fe65f71a58d10f47fed852c2444
Angelica Pilarca & Kim Stephenson & Paula Lee <pair+apilarca.kimstephenson.pdlee877@devbootcamp.com>
2016-09-21 15:57:26 -0700

    seed database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   has_many :votes, foreign_key: :voter_id
   has_many :questions, foreign_key: :author_id
@@ -5,4 +7,15 @@ class User < ActiveRecord::Base
   has_many :responses, foreign_key: :responder_id
 
   validates :username, :email, :password_hash, presence: :true
+
+  include BCrypt
+
+   def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
