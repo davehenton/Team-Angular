3c0c0bf2b8399c48b0cdcbcea248b496f1009354
Angelica Pilarca & Kim Stephenson & Paula Lee <pair+apilarca.kimstephenson.pdlee877@devbootcamp.com>
2016-09-22 11:36:51 -0700

    add responses to questions and answers

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,50 +1,59 @@
-get '/answers/:answer_id/responses' do 
+get '/answers/:answer_id/responses' do
   @answer = Answer.find(params[:answer_id])
   @responses = @answer.responses
   erb :'responses/index'
 end
 
 
-get '/answers/:answer_id/responses/new' do 
+get '/answers/:answer_id/responses/new' do
   @answer = Answer.find(params[:answer_id])
   erb :'responses/new'
 end
 
-post '/answers/:answer_id/responses' do 
-  @answer = Answer.find(params[:answer_id])
-  @response = @answer.responses.new(params[:response])
-  if @response.save
-    redirect "/answers/#{@answer.id}/responses"
-  else
-    erb :'responses/new' 
+post '/questions/:question_id/responses' do
+  @question = Question.find(params[:question_id])
+  response = @question.responses.new(content: params[:content], responder_id: current_user.id )
+  if current_user
+    response.save
   end
+    redirect "/questions/#{params[:question_id]}"
 end
 
-
-get '/answers/:answer_id/responses/:id' do 
+post '/questions/:question_id/answers/:answer_id/responses' do
   @answer = Answer.find(params[:answer_id])
-  @response = @answer.responses.find(params[:id])
-  erb :'responses/show'
+  response = @answer.responses.new(content: params[:content], responder_id: current_user.id )
+  if current_user
+    response.save
+  end
+    redirect "/questions/#{params[:question_id]}"
 end
 
 
-get '/answers/:answer_id/responses/:id/edit' do
-  @answer = Answer.find(params[:answer_id])
-  @response = @answer.responses.find(params[:id])
-  erb :'responses/edit'
-end
+
+# get '/answers/:answer_id/responses/:id' do
+#   @answer = Answer.find(params[:answer_id])
+#   @response = @answer.responses.find(params[:id])
+#   erb :'responses/show'
+# end
+
+
+# get '/answers/:answer_id/responses/:id/edit' do
+#   @answer = Answer.find(params[:answer_id])
+#   @response = @answer.responses.find(params[:id])
+#   erb :'responses/edit'
+# end
 
 put '/answers/:answer_id/responses/:id' do
   @answer = Answer.find(params[:answer_id])
   @response = @answer.responses.find(params[:id])
   if @response.update_attributes(params[:response])
     redirect "/answers/#{@answer.id}/responses"
-  else 
-    erb :'responses/edit' 
+  else
+    erb :'responses/edit'
   end
 end
 
-delete '/answers/:answer_id/responses/:id' do 
+delete '/answers/:answer_id/responses/:id' do
   @answer = Answer.find(params[:answer_id])
   @response = @answer.responses.find(params[:id])
   @response.destroy
